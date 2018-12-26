cd9fcf68022bbf05c67d98f8afad74ef898f1dcd
srinathck <srinathck@github.com>
2018-12-26 07:30:15 -0600

    Initial. EF Tutorials

---
+++ EFBasicTutorials/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+// to use EntityConnection
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Core;
+
+namespace EFBasicTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*** EntityFramework Basic Tutorials Demo Start ***");
+            Console.WriteLine("");
+
+            EFBasics.DynamicProxyDemo();
+            EFBasics.FindEntityDemo();
+            EFBasics.LazyLoadingDemo();
+            EFBasics.ExplicitLoadingDemo();
+            EFBasics.RawSQLtoEntityTypeDemo();
+            EFBasics.ReadDataUsingStoredProcedure();
+            EFBasics.RawSQLCommandDemo();
+            EFBasics.EntitySQLDemo();
+            EFBasics.EntitySQLUsingEntityConnectionDemo();
+            EFBasics.SpatialDataTypeDemo();
+            EFBasics.GetPropertyValuesDemo();
+            EFBasics.LocalDemo();
+            EFBasics.ChangeTrackingDemo();
+            EFBasics.SetValuesDemo();
+            EFBasics.ValidationErrorDemo();
+            EFBasics.CRUDOperationInConnectedScenarioDemo();
+            EFBasics.OptimisticConcurrencyDemo();
+            EFBasics.DBEntityEntryDemo();
+            //Add and map sp_InsertStudentInfo, sp_UpdateStudent 
+            //and sp_DeleteStudent into EDM to run following demo
+            //EFBasics.CUDOperationUsingStoredProcedureDemo();
+
+            AddDemo.AddSingleEntity();
+            AddDemo.AddEntityGraph();
+
+            UpdateDemo.UpdateSingleEntity();
+            UpdateDemo.UpdateEntityGraphUsingId();
+
+
+            //Implement IEntityObjectState in Standard & Teacher entity class to run following demo
+            //UpdateDemo.UpdateEntityGraphUsingStateProperty();
+
+            Console.WriteLine("*** EntityFramework Basic Tutorials Demo Finished ***");
+            Console.ReadKey();
+
+        }
+
+      
+
+        
+    }
+}
---
+++ EFBasicTutorials/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿Instruction to run this demo tutorials:
+- Attach SchoolDB.mdf in your local MS SQL Server 2012
+- If you have MS SQL Server 2008 or 2008 R2 then execute respective scripts included in the project: SchoolDBScript-MSSQL2008.sql or SchoolDBScript-MSSQL2008R2.sql
+- Verify connection string in App.config and make sure that it is pointing to correct server & database. 
+- You may delete School.exmx and add new one after creating SchoolDB database in your local server.
